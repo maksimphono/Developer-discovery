e668d045793ff269d8b222fb11e225df0a28c3cf
maksimphono <trukhinmaksimof@mail.ru>
2025-03-05 02:25:20 +0800

    data processing initial

---
+++ data_processing/consts.py
@@ -0,0 +1,4 @@
+USER_PROFILES_PATH = "/home/trukhinmaksim/Maksim/学习/Thesis/data/user_profiles"
+USER_PROJ_PARTICIPATE_PATH = "/home/trukhinmaksim/Maksim/学习/Thesis/data/user_proj_participate"
+
+USER_PROFILES_IGNORE_LIST = []
---
+++ data_processing/main.py
@@ -0,0 +1,10 @@
+from consts import *
+import os
+
+
+
+def main():
+
+
+
+main()
