74027598ffa97c7620d536e6a60dd6e0722f0a7d
maksimphono <trukhinmaksimof@mail.ru>
2025-03-11 06:58:51 +0800

    updated methods to read data from CSV and JSON databases, code refactored

--- data_processing/consts.py
+++ data_processing/consts.py
@@ -1,6 +1,1 @@
-USER_PROFILES_PATH = "/home/trukhinmaksim/Maksim/学习/Thesis/data/user_profiles"
-USER_PROJ_PARTICIPATE_PATH = "/home/trukhinmaksim/Maksim/学习/Thesis/data/user_proj_participate"
-OUTPUT_DIRECTORY = "/home/trukhinmaksim/Maksim/学习/Thesis/data/output"
 
-USER_PROFILES_IGNORE_LIST = []
-LOG_DIRECTORY = ""
--- data_processing/main.py
+++ data_processing/main.py
@@ -2,12 +2,51 @@ from consts import *
 import os
 import json
 from copy import deepcopy
+import re
+
+SCANNED_FILES_LIST_PATH = "/home/trukhinmaksim/Maksim/学习/Thesis/code/docker/container/src/data_processing/read_files_list.txt"
+USER_PROFILES_PATH = "/home/trukhinmaksim/Maksim/学习/Thesis/data/user_profiles"
+USER_PROJ_PARTICIPATE_PATH = "/home/trukhinmaksim/Maksim/学习/Thesis/data/user_proj_participate"
+OUTPUT_DIRECTORY = "/home/trukhinmaksim/Maksim/学习/Thesis/data/output/killme"
+
+USER_PROFILES_IGNORE_LIST = []
+LOG_PATH = "/home/trukhinmaksim/Maksim/学习/Thesis/code/docker/container/src/data_processing/logs.txt"
 
 users_db: dict[dict] = {}
 projects_db: dict[dict] = {}
-readFilesNames = []
+scannedFilesNames = []
 
-stats = {"user_number" : 0, "proj_number" : 0}
+class Stats:
+    def __init__(self, logFilePath = LOG_PATH, append = True):
+        self.logs = []
+        self.logFilePath = logFilePath
+        self.logsAppend = append
+        self.stats = {"total_user_number" : 0, "total_proj_number" : 0}
+
+    def log(self, string):
+        self.logs.append(str(string))
+
+    def flash(self):
+        with open(self.logFilePath, "a+" if self.logsAppend else "w", encoding="utf-8") as file:
+            file.write("\n".join(self.logs))
+            file.write("\n")
+
+        self.logs.clear()
+
+    def logDBinfo(self):
+        self.log("Stats about databases:")
+        self.log(f"\tScanned {len(users_db)} users")
+        self.log(f"\tScanned {len(projects_db)} projects")
+        self.stats["total_user_number"] += len(users_db)
+        self.stats["total_proj_number"] += len(projects_db)
+
+    def logFinal(self):
+        self.log(f"\nTotal users number scanned: {self.stats["total_user_number"]}")
+        self.log(f"Total projects number scanned: {self.stats["total_proj_number"]}")
+        self.log(f"\tScanned CSV files:\n\t\t{str(ScannedFilesManager.scannedFilesNames)}")
+        self.log("END")
+        self.flash()
+
 
 class UserDataCreator:
     attr_list = ["id", "type", "projects"]
@@ -31,54 +70,68 @@ class UserDataCreator:
         return obj
 
 
-def getProjectsDataFromDict(dictObj) -> dict:
+class ProjectDataCreator:
+    attr_list = ["id"]
+    default_values = ["0"] # U = unknown 
+
+    class many:
+        @classmethod
+        def fromDict(cls, dictObj : dict) -> list:
+            objs = []
+            for project_id in dictObj["projects"].keys():
+                objs.append(dict({"id" : project_id}))
+            return objs
+
+def getProjectsDataFromDict(dictObj) -> list:
     objs = []
     for project_id in dictObj["projects"].keys():
         objs.append(dict({"id" : project_id}))
     return objs
 
-def readCSVDatabase(priorityFiles = [], limit = 2):
+def readCSVDatabase(priorityFiles = [], limit = 2) -> str:
     ALLOWED_USER_TYPES = ["A", "B"]
     count = 0
+    filesNames = []
     # read data about users from the the CSV file
 
     def readFile(root, fileName):
-        nonlocal count
+        nonlocal count, filesNames
         print("Reading file ", fileName)
+        filesNames.append(fileName)
 
         with open(os.path.join(root, fileName), encoding="utf-8") as file:
             file.readline()
             lines = file.readlines()
             for userData in filter(lambda user: user["type"] in ALLOWED_USER_TYPES, map(UserDataCreator.fromCSV, lines)):
                 users_db[userData["id"]] = userData
-                if len(users_db) == 40: 
-                    pass # killme
-        readFilesNames.append(fileName)
+
+        ScannedFilesManager.add(fileName)
         count += 1
 
     # read priority files first 
     for fileName in priorityFiles:
         if count >= limit: break
-        if fileName not in USER_PROFILES_IGNORE_LIST and fileName not in readFilesNames:
+        if fileName not in USER_PROFILES_IGNORE_LIST and fileName not in ScannedFilesManager.scannedFilesNames:
             readFile(USER_PROFILES_PATH, fileName)
 
     # read other files
     for root, dirs, files in os.walk(USER_PROFILES_PATH):
         for fileName in files:
             if count >= limit: break
-            if fileName not in USER_PROFILES_IGNORE_LIST and fileName not in readFilesNames:
+            if fileName not in USER_PROFILES_IGNORE_LIST and fileName not in ScannedFilesManager.scannedFilesNames:
                 readFile(root, fileName)
 
         # don't need to go through subdirectories
+    return filesNames
 
 def readJSONDatabase(filesNamesToRead):
     data: str = ""
-    usersProfiles: list = []
+
     for fileName in filesNamesToRead:
-        with open(os.path.join(USER_PROJ_PARTICIPATE_PATH, fileName), encoding="utf-8", ) as file:
+        with open(os.path.join(USER_PROJ_PARTICIPATE_PATH, fileName), encoding="utf-8") as file:
             for line in file.readlines():
                 data: dict = json.loads(line)
-                projectsDataList: list[dict] = getProjectsDataFromDict(data)
+                projectsDataList: list[dict] = ProjectDataCreator.many.fromDict(data) #getProjectsDataFromDict(data)
                 user_id: str = data["user_id"]
                 userProjects: list[dict] = data["projects"]
 
@@ -88,8 +141,8 @@ def readJSONDatabase(filesNamesToRead):
                         if users_db[user_id]["projects"] == userProjects:
                             print(f"Eq, {user_id}")
                         else:
-                            print(f"nEq, {user_id}")
-                        #raise Exception(f"User {user_id} already has data about his projects")
+                            #print(f"nEq, {user_id}")
+                            raise Exception(f"User {user_id} already has data about his projects")
 
                     # set the projects dictionary for that user
                     users_db[user_id]["projects"] = deepcopy(userProjects)
@@ -102,46 +155,75 @@ def readJSONDatabase(filesNamesToRead):
                             projects_db[id] = deepcopy(projData)
 
 
-
-def test():
-    users_db["github:13627491210"] = UserDataCreator.fromCSV("github:13627491210,A\n")
-    #userData = UserDataCreator.fromDict({"id" : })
-    readJSONDatabase(["user_profiles_github_23mf_react-native-translucent-modal.json"])
-
-
 def saveDatabases(database_id):
     with open(os.path.join(OUTPUT_DIRECTORY, f"users_db_{database_id}.json"), "w", encoding="utf-8") as file:
         json.dump(users_db, fp = file, ensure_ascii=False, indent=4, separators = (",", ":"))
-    with open(os.path.join(OUTPUT_DIRECTORY, f"project_db_{database_id}.json"), "w", encoding="utf-8") as file:
+    with open(os.path.join(OUTPUT_DIRECTORY, f"projects_db_{database_id}.json"), "w", encoding="utf-8") as file:
         json.dump(projects_db, fp = file, ensure_ascii=False, indent=4, separators = (",", ":"))
 
     users_db.clear()
     projects_db.clear()
 
+class ScannedFilesManager:
+    scannedFilesNames = []
+    @classmethod
+    def get(cls) -> list:
+        with open(SCANNED_FILES_LIST_PATH, encoding="utf-8") as file:
+            cls.scannedFilesNames = list(map(lambda s: s[:-1] if s[-1] == "\n" else s, file.readlines()))
 
+        return cls.scannedFilesNames
+
+    @classmethod
+    def update(cls):
+        with open(SCANNED_FILES_LIST_PATH, "w", encoding="utf-8") as file:
+            for filePath in cls.scannedFilesNames:
+                print(filePath, file = file)
+
+    @classmethod
+    def add(cls, name):
+        cls.scannedFilesNames.append(name)
+
+
+def getScannedFiles():
+    with open(SCANNED_FILES_LIST_PATH, encoding="utf-8") as file:
+        return list(file.readlines())
+    
+#def updateScannedFiles():
+#    with open(SCANNED_FILES_LIST_PATH, "a+", encoding="utf-8") as file:
+#        [file.write(filePath) for filePath in scannedFilesNames[]]
 
 def main():
-    NUMBER_FILES_TO_SCAN = 4
+    # separating scanning logic, by scanning small amount of files at once
+    PORTION_AMOUNT = 2
+    FILES_PER_PORTION = 3 # in total, (PORTION_AMOUNT * FILES_PER_PORTION) files will be scanned
     PRIORITY_CSV_FILES = [
-        "user_profiles_github_23mf_react-native-translucent-modal.csv",
-        "user_profiles_github_2017398956_react-native-textinput-maxlength-fixed.csv",
-        "user_profiles_github_a7ul_react-native-exception-handler.csv",
-        "user_profiles_github_afollestad_material-dialogs.csv"
+        #"user_profiles_github_23mf_react-native-translucent-modal.csv",
+        #user_profiles_github_2017398956_react-native-textinput-maxlength-fixed.csv",
+        #"user_profiles_github_a7ul_react-native-exception-handler.csv",
+        #"user_profiles_github_afollestad_material-dialogs.csv"
     ]
 
-    for i in range(NUMBER_FILES_TO_SCAN):
-        readCSVDatabase(PRIORITY_CSV_FILES, 1)
-        readJSONDatabase(map(lambda fn: fn.replace(".csv", ".json"), readFilesNames))
+    stats = Stats()
+    ScannedFilesManager.get()
 
-        stats["user_number"] += len(users_db)
-        stats["proj_number"] += len(projects_db)
+    stats.log("\nSTART")
+
+    for i in range(PORTION_AMOUNT):
+        filesNames = readCSVDatabase(PRIORITY_CSV_FILES, FILES_PER_PORTION)
+        readJSONDatabase(map(lambda fn: fn.replace(".csv", ".json"), filesNames))
+
+        print(f"Scanned {len(users_db)} users")
+        print(f"Scanned {len(projects_db)} projects")
+        #stats.logDBinfo()
 
         saveDatabases(i)
 
-    with open("read_files_list.txt", "w") as file:
-        file.write("\n".join(readFilesNames))
-    print("Total number of users: {0}\nTotal number of projects: {1}".format(stats["user_number"], stats["proj_number"]))
+    ScannedFilesManager.update()
 
+    #with open("read_files_list.txt", "w") as file:
+    #    file.write("\n".join(scannedFilesNames))
+
+    stats.logFinal()
 
 
 main()
