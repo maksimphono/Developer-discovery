bfdbc76ea55b6ff8b4f49d585c419823d5f2bdd4
maksimphono <trukhinmaksimof@mail.ru>
2025-04-07 07:25:04 +0000

    style: changed logic for cache analysis

--- src/preprocessings.py
+++ src/preprocessings.py
@@ -92,7 +92,7 @@ def extractScannedUsers(data):
 
 cacheFileName = "cache__02-04-2025__(good)_{0}.json"
 
-adapter = JSONMultiFileAdapter(cacheFileName, 320)
+adapter = JSONMultiFileAdapter(cacheFileName, 400)
 #adapter = JSONAdapter(cacheFileName)
 
 ProjectsDatasetManager.usersCollection = usersCollection
@@ -123,7 +123,7 @@ counter = 0
 startPoint = time()
 
 try:
-    for i in range(320, 350): #60, 70
+    for i in range(400, 420): #60, 70
         manager.fromDB()
         manager.preprocess()
 
