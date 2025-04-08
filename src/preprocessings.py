bd78a786efe914a14cd4253552a8f05721b3d483
maksimphono <trukhinmaksimof@mail.ru>
2025-04-08 10:29:09 +0000

    fix: fixed bug when I can use the 'autoTuner' class with "objective" function from outside, but can't with this method

--- src/preprocessings.py
+++ src/preprocessings.py
@@ -92,7 +92,7 @@ def extractScannedUsers(data):
 
 cacheFileName = "cache__02-04-2025__(good)_{0}.json"
 
-adapter = JSONMultiFileAdapter(cacheFileName, 570)
+adapter = JSONMultiFileAdapter(cacheFileName, 650)
 #adapter = JSONAdapter(cacheFileName)
 
 ProjectsDatasetManager.usersCollection = usersCollection
@@ -123,7 +123,7 @@ counter = 0
 startPoint = time()
 
 try:
-    for i in range(570, 590): #60, 70
+    for i in range(650, 700): #60, 70
         manager.fromDB()
         manager.preprocess()
 
