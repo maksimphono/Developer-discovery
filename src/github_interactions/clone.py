f15e91c49cfb4c0d7124005f52e3f254a4676079
maksimphono <trukhinmaksimof@mail.ru>
2025-04-30 22:31:40 +0000

    feat: completed logic for readme file extraction using github API

--- src/github_interactions/clone.py
+++ src/github_interactions/clone.py
@@ -10,6 +10,8 @@ import aiohttp
 import markdown
 import re
 from lxml import html
+from random import random
+import json
 
 from src.utils.CacheAdapter import FlatAdapter
 
@@ -20,7 +22,9 @@ with open("/home/trukhinmaksim/src/environment.json") as file:
 
 tokens = TOKENS
 currentToken = tokens[0]
-PORTION_SIZE = 2
+PORTION_SIZE = 10
+SKIP = 16590 + 16030 + 1570
+INITIAL_TOKENS_ROTATION = 4
 
 def decodeFile(fileData : str) -> str:
     return base64.b64decode(fileData).decode('utf-8')
@@ -88,30 +92,48 @@ EXP_TOKEN_EXHAUSTED = Exception("TOKEN EXHAUSTED")
 
 i = 0
 
-async def fetch(session, url):
-    global i
-    #if i == 2: return EXP_TOKEN_EXHAUSTED
+async def fetch(session, url, retryingAttempt = 0):
+    try:
+        async with session.get(url) as response:
+            if response.status == 200:
+                try:
+                    content = (await response.json())["content"]
+                    content = decodeFile(content)
+                    return {"content" : content, "remain" : int(response.headers["X-RateLimit-Remaining"]), "exhausted" : False}
+                except KeyError:
+                    return {"content" : "", "remain" : int(response.headers["X-RateLimit-Remaining"]), "exhausted" : False}
 
-    async with session.get(url) as response:
-        if response.status == 200:
-            content = (await response.json())["content"]
-            content = decodeFile(content)
+                #print(f"{url} fetched, i = {i}")
 
-            #print(f"{url} fetched, i = {i}")
-            return {"content" : content, "remain" : int(response.headers["X-RateLimit-Remaining"]), "exhausted" : False}
+            else:
+                #print(f"Got response code = {response.status}")
+                if response.status == 403 or response.status == 429: # rate limit exceeded
+                    print(f"Got response code = {response.status}")
+                    return {"content" : "", "remain" : 0, "exhausted" : True, "reset" : int(response.headers["X-RateLimit-Reset"])}
+                if response.status == 404:
+                    print(f"Not found: {str(url)}")
+                return {"content" : "", "remain" : 0, "exhausted" : False, "remain" : int(response.headers["X-RateLimit-Remaining"])}
+
+                #raise Exception("Error while fitch")
+            #return await response.json()
+    except TimeoutError as err:
+        if retryingAttempt < 3:
+            t = int(5 * (retryingAttempt + 1) + random() * 15)
+            print(f"TimeoutError catched ({retryingAttempt}), retrying in {t}")
+            await asyncio.sleep(t)
+            return await fetch(session, url, retryingAttempt + 1)
         else:
-            print(f"Got response code = {response.status}")
-            if response.status == 403 or response.status == 429: # rate limit exceeded
-                return {"content" : "", "remain" : 0, "exhausted" : True, "reset" : int(response.headers["X-RateLimit-Reset"])}
-            return {"content" : "", "remain" : 0, "exhausted" : False, "remain" : int(response.headers["X-RateLimit-Remaining"])}
+            raise err
 
-            #raise Exception("Error while fitch")
-        #return await response.json()
-
-async def rotateTokens():
+async def rotateTokens(enableSleep = True):
     tokens.append(tokens.pop(0))
     print(f"Rotate tokens; token : {tokens[0]}")
+
+    if enableSleep:
+        await asyncio.sleep(int(12 + random() * 15))
+
     return tokens
+
 
 
 async def fetchWithClientSession(tokens, urls = list()):
@@ -123,14 +145,13 @@ async def fetchWithClientSession(tokens, urls = list()):
             #print(f"Prepare to fetch {url}")
             tasks.append(fetch(session, url + "/contents/README.md"))
 
-        #tasks = [fetch(session, url + "/contents/README.md") for url in urls[:1]]
         results = await asyncio.gather(*tasks)
 
         if results[-1]["remain"] < PORTION_SIZE: # if the token will be exhausted on the next portion
             if results[-1]["exhausted"]:
                 # token exhausted, wait until it resets and try again
                 print(f"Token will reset in {results[-1]['reset'] - time()} s, sleeping...")
-                sleep(abs(results[-1]["reset"] - time()) + 10)
+                asyncio.sleep(abs(results[-1]["reset"] - time()) + 10)
                 return await fetchWithClientSession(tokens, urls) # try again after sleep (potentially token must reset during that time)
             else:
                 await rotateTokens()
@@ -142,30 +163,43 @@ async def main():
     print("Welcome")
 
     readmeAdapter = FlatAdapter("/home/trukhinmaksim/src/data/cache_30-04-25/raw_readme_30-04-25")
+    print("Adapter created on /home/trukhinmaksim/src/data/cache_30-04-25/raw_readme_30-04-25")
     start = time()
     urlsLst = []
     ids = []
+
     with open("/home/trukhinmaksim/src/data/cache_30-04-25/urls.json", "r", encoding = "utf-8") as file:
         urlsLst = json.load(fp = file)
     
     with open("/home/trukhinmaksim/src/data/cache_30-04-25/ids.json", "r", encoding = "utf-8") as file:
         ids = json.load(fp = file)
 
-    #currentToken = tokens[0]
+    print(f"urlsLst = {urlsLst[:5]}")
+    print(f"ids = {ids[:5]}")
+    if len(ids) != len(urlsLst):
+        print("Different len! Exiting")
+        return
+
+    for j in range(INITIAL_TOKENS_ROTATION):
+        await rotateTokens(False)
+
+    print(tokens[0])
     i = 0
     counter = 0
+    skip = SKIP
     start = time() #len(urlsLst)
     try:
-        for j in range(0, len(urlsLst), PORTION_SIZE):
+        print(f"Starting from {skip}")
+        for j in range(skip, len(urlsLst), PORTION_SIZE):
             results = await fetchWithClientSession(tokens, urls = urlsLst[j:j + PORTION_SIZE])
             d = zip(ids[j:j + PORTION_SIZE], results)
             #print([r[:10] for r in results])
-            readmeAdapter.write([{"proj_id" : proj_id, "readme" : readme} for proj_id, readme in d])
+            readmeAdapter.save([{"proj_id" : proj_id, "readme" : readme} for proj_id, readme in d])
             counter += len(results)
 
             if j % 1000 == 0:
-                print(f"Scanning {j}'th repo ({urlsLst[j]}); token : {tokens[0]}")
-                print(f"Content: {results[0][:20]}\n")
+                print(f"Scaned {j + PORTION_SIZE} repos; last: ({urlsLst[j + PORTION_SIZE]}); token : {tokens[0]}")
+                print(f"Content: {results[-1][:20]}\n")
                 print(f"Total {counter} readme files were saved in {time() - start} s")
 
     except Exception as exp:
