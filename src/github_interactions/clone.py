d95e1fbc14a4c00ccd222a14df740c928bae3459
maksimphono <trukhinmaksimof@mail.ru>
2025-05-01 08:17:00 +0000

    feat: created mechanism to recover from 403 error if access is blocked and added better mechanism to check if token exhausted or not

--- src/github_interactions/clone.py
+++ src/github_interactions/clone.py
@@ -12,8 +12,17 @@ import re
 from lxml import html
 from random import random
 import json
+import logging
 
 from src.utils.CacheAdapter import FlatAdapter
+
+#30-04-25_snan_readme(403).log
+
+logging.basicConfig(
+    filename="/home/trukhinmaksim/src/logs/01-05-25_scan_readme.log",
+    format='%(asctime)s : %(levelname)s : %(message)s',
+    level=logging.INFO
+)
 
 TOKEN = ""
 
@@ -23,8 +32,8 @@ with open("/home/trukhinmaksim/src/environment.json") as file:
 tokens = TOKENS
 currentToken = tokens[0]
 PORTION_SIZE = 10
-SKIP = 16590 + 16030 + 1570 + 2830
-INITIAL_TOKENS_ROTATION = 4
+SKIP = 70320 + 40770 + 300 + 4230 + 3300 #16590 + 16030 + 1570 + 2830
+INITIAL_TOKENS_ROTATION = 0
 
 def decodeFile(fileData : str) -> str:
     return base64.b64decode(fileData).decode('utf-8')
@@ -99,19 +108,27 @@ async def fetch(session, url, retryingAttempt = 0):
                 try:
                     content = (await response.json())["content"]
                     content = decodeFile(content)
-                    return {"content" : content, "remain" : int(response.headers["X-RateLimit-Remaining"]), "exhausted" : False}
+                    return {"content" : content, "remain" : int(response.headers["X-RateLimit-Remaining"]), "exhausted" : False, "reset" : int(response.headers["X-RateLimit-Reset"])}
                 except KeyError:
-                    return {"content" : "", "remain" : int(response.headers["X-RateLimit-Remaining"]), "exhausted" : False}
+                    return {"content" : "", "remain" : int(response.headers["X-RateLimit-Remaining"]), "exhausted" : False, "reset" : int(response.headers["X-RateLimit-Reset"])}
 
                 #print(f"{url} fetched, i = {i}")
 
             else:
                 #print(f"Got response code = {response.status}")
                 if response.status == 403 or response.status == 429: # rate limit exceeded
-                    print(f"Got response code = {response.status}")
-                    return {"content" : "", "remain" : 0, "exhausted" : True, "reset" : int(response.headers["X-RateLimit-Reset"])}
+                    logging.info(f"Got response code = {response.status} ({url})")
+                    print(f"Got response code = {response.status} ({url})")
+                    print({"reset" : int(response.headers["X-RateLimit-Reset"]), "message" : (await response.json())["message"]})
+
+                    if input(">>> ") == "s":
+                        return {"content" : "", "remain" : int(response.headers["X-RateLimit-Remaining"]), "exhausted" : False, "reset" : int(response.headers["X-RateLimit-Reset"])}
+                    else:
+                        return {"content" : "", "remain" : 0, "exhausted" : True, "reset" : int(response.headers["X-RateLimit-Reset"])}
+
                 if response.status == 404:
                     print(f"Not found: {str(url)}")
+                    logging.info(f"Not found: {str(url)}")
                 return {"content" : "", "remain" : 0, "exhausted" : False, "remain" : int(response.headers["X-RateLimit-Remaining"])}
 
                 #raise Exception("Error while fitch")
@@ -120,6 +137,7 @@ async def fetch(session, url, retryingAttempt = 0):
         if retryingAttempt < 3:
             t = int(5 * (retryingAttempt + 1) + random() * 15)
             print(f"TimeoutError catched ({retryingAttempt}), retrying in {t}")
+            logging.info(f"TimeoutError catched ({retryingAttempt}), retrying in {t}")
             await asyncio.sleep(t)
             return await fetch(session, url, retryingAttempt + 1)
         else:
@@ -128,6 +146,7 @@ async def fetch(session, url, retryingAttempt = 0):
 async def rotateTokens(enableSleep = True):
     tokens.append(tokens.pop(0))
     print(f"Rotate tokens; token : {tokens[0]}")
+    logging.info(f"Rotate tokens; token : {tokens[0]}")
 
     if enableSleep:
         await asyncio.sleep(int(12 + random() * 15))
@@ -136,7 +155,7 @@ async def rotateTokens(enableSleep = True):
 
 
 
-async def fetchWithClientSession(tokens, urls = list()):
+async def fetchWithClientSession(tokens, urls = list(), tokenRotateCount = 0):
     currentToken = tokens[0]
     tasks = list()
     async with aiohttp.ClientSession(headers={"Authorization": f"token {currentToken}"}) as session:
@@ -148,11 +167,16 @@ async def fetchWithClientSession(tokens, urls = list()):
         results = await asyncio.gather(*tasks)
 
         if results[-1]["remain"] < PORTION_SIZE: # if the token will be exhausted on the next portion
-            if results[-1]["exhausted"]:
+            if any((result["exhausted"] for result in results)):
                 # token exhausted, wait until it resets and try again
-                print(f"Token will reset in {results[-1]['reset'] - time()} s, sleeping...")
-                asyncio.sleep(abs(results[-1]["reset"] - time()) + 10)
-                return await fetchWithClientSession(tokens, urls) # try again after sleep (potentially token must reset during that time)
+                if tokenRotateCount >= 5:
+                    print(f"Token will reset in {results[-1]['reset'] - time()} s, sleeping...")
+                    logging.info(f"Token will reset in {results[-1]['reset'] - time()} s, sleeping...")
+                    await asyncio.sleep(abs(results[-1]["reset"] - time()) + 10)
+                    return await fetchWithClientSession(tokens, urls, 0) # try again after sleep (potentially token must reset during that time)
+                else:
+                    await rotateTokens()
+                    return await fetchWithClientSession(tokens, urls, tokenRotateCount + 1) # try again after sleep (potentially token must reset during that time)
             else:
                 await rotateTokens()
 
@@ -161,9 +185,11 @@ async def fetchWithClientSession(tokens, urls = list()):
 
 async def main():
     print("Welcome")
+    logging.info("Welcome")
 
     readmeAdapter = FlatAdapter("/home/trukhinmaksim/src/data/cache_30-04-25/raw_readme_30-04-25")
     print("Adapter created on /home/trukhinmaksim/src/data/cache_30-04-25/raw_readme_30-04-25")
+    logging.info("Adapter created on /home/trukhinmaksim/src/data/cache_30-04-25/raw_readme_30-04-25")
     start = time()
     urlsLst = []
     ids = []
@@ -186,29 +212,35 @@ async def main():
     print(tokens[0])
     i = 0
     counter = 0
+    totalCounter = SKIP
     skip = SKIP
     start = time() #len(urlsLst)
     try:
         print(f"Starting from {skip}")
+        logging.info(f"Starting from {skip}")
         for j in range(skip, len(urlsLst), PORTION_SIZE):
             results = await fetchWithClientSession(tokens, urls = urlsLst[j:j + PORTION_SIZE])
             d = zip(ids[j:j + PORTION_SIZE], results)
             #print([r[:10] for r in results])
             readmeAdapter.save([{"proj_id" : proj_id, "readme" : readme} for proj_id, readme in d])
             counter += len(results)
+            totalCounter += len(results)
 
             if j % 1000 == 0:
-                print(f"Scaned {j + PORTION_SIZE} repos; last: ({urlsLst[j + PORTION_SIZE - 1]}); token : {tokens[0]}")
+                print(f"Scaned {totalCounter} repos in total; last: ({urlsLst[totalCounter - 1]}); token : {tokens[0]}")
                 print(f"Content: {results[-1][:20]}\n")
-                print(f"Total {counter} readme files were saved in {time() - start} s")
+                print(f"{counter} readme files were saved in {time() - start} s")
+                logging.info(f"Scaned {totalCounter} repos; last: ({urlsLst[totalCounter - 1]}); token : {tokens[0]}\nContent: {results[-1][:20]}\n\nTotal {counter} readme files were saved in {time() - start} s\n")
 
     except Exception as exp:
         print(f"Got exception during execution:", str(exp))
+        logging.info(f"Got exception during execution:", str(exp))
         raise exp
     finally:
         #print(*results, sep = "\n" * 4)
-        print(f"Saved {counter} readme files")
+        print(f"Saved {counter} readme files during execution, saved in total: {totalCounter}")
         print(f"Process completed in {time() - start}")
+        logging.info(f"Saved {counter} readme files during execution, saved in total: {totalCounter}\nProcess completed in {time() - start}")
 
 
 url = f"https://api.github.com/repos/{owner}/{repo}"
