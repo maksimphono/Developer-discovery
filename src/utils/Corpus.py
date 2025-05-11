724dc9f0ac16645a5aee0a7b1f49f31bf1a52f5a
maksimphono <trukhinmaksimof@mail.ru>
2025-05-11 15:21:57 +0000

    feat: created simple corpus for BERT model (replaces 'Dataset' class)

--- src/utils/Corpus.py
+++ src/utils/Corpus.py
@@ -20,6 +20,8 @@ from src.utils.helpers import flatternData
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from transformers import BertTokenizer
+from transformers.tokenization_utils_base import BatchEncoding
 
 class Corpus:
     # base class for every data corpus, that will be used by model
@@ -161,6 +163,30 @@ class Doc2VecCorpus(MemoryCorpus):
             self.workingList = self.dataOnlyID
         else:
             self.workingList = self.data
+
+class SBertCorpus(MemoryCorpus):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    @classmethod
+    def createTaggedDocument(cls, words : str, tags : list, *args, **kwargs):
+        encoding = cls.tokenizer(words, *args, **kwargs)
+        encoding["tags"] = list(tags)
+
+        return encoding
+
+    def __init__(self, adapter = None, limit = np.inf, includeOnlyID = True, max_len = 128):
+        super().__init__(adapter, limit, includeOnlyID)
+        self.max_len = max_len
+        self.data = tuple([SBertCorpus.createTaggedDocument(words = doc["text"], tags = doc["tags"], return_tensors='pt', truncation=True, padding='max_length', max_length=self.max_len) for doc in adapter.load(limit)])
+        self.len = len(self.data)
+        self.workingList = self.data
+
+    def __len__(self):
+        return len(self.workingList)
+
+    def __getitem___(self, index):
+        return self.workingList[index]
+
 
 class PairsCorpus(MemoryCorpus):
     def __init__(self, adapter = None, limit = np.inf, includeOnlyID = True):
