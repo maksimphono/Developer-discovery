fe011b60f89690002bdfa784c50077effbd50474
maksimphono <trukhinmaksimof@mail.ru>
2025-05-24 17:46:52 +0000

    feat: created simple class to translate readme files asynchronously, created scripts to run aws instances

--- src/utils/DatasetManager.py
+++ src/utils/DatasetManager.py
@@ -24,11 +24,12 @@ import asyncio
 import nest_asyncio
 from googletrans import Translator
 import traceback
+import fasttext
 import logging
 
-from src.utils.CacheAdapter import JSONAdapter, CacheAdapter, EXP_END_OF_DATA, Factory_21_04_25_HIGH as CacheFactory
+from src.utils.CacheAdapter import JSONAdapter, CacheAdapter, EXP_END_OF_DATA
 from src.data_processing.collect_projects_data import collectOneProjectData, EXP_NOT_IN_DB
-from src.utils.DatabaseConnect import CacheConnector
+from src.utils.DatabaseConnect import DatabaseConnector
 
 def downloadArgosLangPackages(langList = ["es", "pt", "zh", "zt", "ru", "de", "ja", "ko"]):
     argostranslate.package.update_package_index()
@@ -38,6 +39,21 @@ def downloadArgosLangPackages(langList = ["es", "pt", "zh", "zt", "ru", "de", "j
     for pkg in packages:
         argostranslate.package.install_from_path(pkg.download())
 
+try:
+    fastTextModel = fasttext.load_model('/home/trukhinmaksim/lid.176.bin')
+except ValueError as e:
+    print(f"Error loading model: {e}")
+    print("Please ensure 'lid.176.bin' is in the current directory or provide the full path.")
+    print("You can download it from: https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin")
+    exit()
+
+def detectLang(text):
+    global fastTextModel
+    predictions = fastTextModel.predict(text)
+    languageLabel = predictions[0][0]
+    languageCode = languageLabel.replace('__label__', '')
+
+    return languageCode
 
 class IgnoreList(dict):
     def includes(self, item):
@@ -398,10 +414,11 @@ class NewDatasetManager(DatasetManager):
         self.processedProjsIds = []
         self.processedProjectsNum = 0
 
-    def translateText(self, text, retry = 3, useServer = False):
+    def translateText(self, text, retry = 3, useServer = False, precheck = True):
         # will try to use Google Translate, but if any error occures, will use Argos offline translator
         try:
-            if text.isascii() or detect(text) == "en": return text # if the text is already english (either ascii or english with unicode emoji)
+            if precheck and (text.isascii() or detectLang(text) == "en"): 
+                return text # if the text is already english (either ascii or english with unicode emoji)
         except LangDetectException:
             pass
 
@@ -444,9 +461,10 @@ class NewDatasetManager(DatasetManager):
         #print(f"Translation Faled after {retry} attempts, Using Argos for {text[:10]}...\nLanguage detected as: {langCode}")
         #return argostranslate.translate.translate(text, langCode, "en")
 
-    def textPreprocessing(self, text):
+    def textPreprocessing(self, text, translate = True):
         # Translate:
-        text = self.translateText(text, 3, False)
+        if translate: 
+            text = self.translateText(text, 3, False)
         # Remove unicode:
         text = text.encode("ascii", "ignore").decode()
         # Process camel case:
@@ -555,89 +573,126 @@ class NewDatasetManager(DatasetManager):
         self.mappedData.clear()
         self.processedProjsIds.clear()
 
-class ReadmeDatasetManager(NewDatasetManager):
+
+class RawTextDatasetManager(NewDatasetManager):    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #self.inputAdapter = CacheFactory.createFlatAdapter()
-        self.readmeCollection = CacheConnector("mongodb://10.22.48.31:27020/").collection("raw_readme_30-04-25")
-        self.mapper = self.merge
+        self.proj_info = DatabaseConnector("mongodb://readonlyUser:cictest123456@114.212.84.247:27017/", "developer_discovery").collection("proj_info")
 
-    def processMarkdown(self, markdown_content):
-    	text = re.sub(r'^#+\s*', '', markdown_content, flags=re.MULTILINE)
-    
-    	# Remove bold and italic markers (* and **)
-    	text = re.sub(r'\*\*', '', text)
-    	text = re.sub(r'\*', '', text)
-    
-    	# Remove inline code (`)
-    	text = re.sub(r'`', '', text)
-    
-    	# Remove blockquotes (lines starting with '>')
-    	text = re.sub(r'^>\s*', '', text, flags=re.MULTILINE)
-    
-    	# Remove unordered list markers (- or *)
-    	text = re.sub(r'^-+\s*', '', text, flags=re.MULTILINE)
-    
-    	# Remove ordered list markers (numbers followed by .)
-    	text = re.sub(r'^\d+\.\s*', '', text, flags=re.MULTILINE)
-    
-    	# Remove links (both inline and reference style)
-    	text = re.sub(r'\[([^\]]+)\]\(([^)]+)\)', r'\1', text)	# Inline links
-    	text = re.sub(r'\[([^\]]+)\]\[[^\]]+\]', r'\1', text)		# Reference links (without resolving)
-    	text = re.sub(r'^\[[^\]]+\]:\s.+', '', text, flags=re.MULTILINE) # Link definitions
-    
-    	# Remove images (similar to links)
-    	text = re.sub(r'!\[([^\]]*)\]\(([^)]+)\)', r'\1', text)
-    	text = re.sub(r'!\[([^\]]*)\]\[[^\]]+\]', r'\1', text)
-    
-    	# Remove horizontal rules (---, ___, ***)
-    	text = re.sub(r'^-{3,}\s*$', '', text, flags=re.MULTILINE)
-    	text = re.sub(r'^_{3,}\s*$', '', text, flags=re.MULTILINE)
-    	text = re.sub(r'^\*{3,}\s*$', '', text, flags=re.MULTILINE)
-    
-    	# Remove code blocks (```) - simple removal, might leave surrounding text
-    	text = re.sub(r'```.*?```', '', text, flags=re.DOTALL)
-    
-    	# Remove HTML tags (if any are present in the Markdown) - basic removal
-    	text = re.sub(r'<[^>]+>', '', text)
-    
-    	# Remove extra whitespace and newlines
-    	text = '\n'.join(line.strip() for line in text.splitlines() if line.strip())
+    def reset(self, skip):
+        self.inputAdapter.reset()
+        if skip > 0:
+            self.inputAdapter.load(skip)
 
-    	return text
+    def prepareData(self, project):
+        proj_id = project["tags"][0]
+        #print(proj_id)
+        projData = self.proj_info.find_one({"proj_id" : proj_id}, projection = {"name" : True, "description" : True})
+        joinedText = ". ".join([projData["name"], projData["description"]])
+        translated = self.translateText(joinedText)
 
-    def translateText(self, text, retryAttept = 3, useServer = False):
-        parts = text.split("\n")
-        translated = []
+        return {"text" : translated, "tags" : project["tags"]}
 
-        for part in parts:
-            translated.append(super().translateText(part, retryAttept, useServer))
-
-        return " ".join(translated)
-
-    def merge(self, doc):
+    def preprocess(self, project):
         while True:
-            # loop for retrying
             try:
-                proj_id = doc["tags"][0]
-                readme = self.readmeCollection.find_one({"proj_id" : proj_id})["readme"]
-                readmeText = self.processMarkdown(readme)
-                preprocessed = self.textPreprocessing(readmeText)
-
-                doc["tokens"].extend(preprocessed)
-                #result = self.projectDataPreprocessing(project)
-                #self.processedProjsIds.append(project["id"])
-                return doc
+                result = self.prepareData(project)
+                return result
             except NewDatasetManager.EXP_CONNECTION_LOSS as exp:
                 if self.handleConnectionLoss():
                     continue # try again
                 else:
-                    self.interruptGracefully(f"Falied to fix error :(\nError occured while scanning {project['proj_id']}")
+                    self.interruptGracefully(f"Falied to fix error :(\nError occured while scanning {project['tags'][0]}")
             except Exception as exp:
                 if self.handleException():
                     continue
                 else:
-                    self.interruptGracefully(f"Falied to fix error :(\nError occured while scanning {project['proj_id']}")
+                    self.interruptGracefully(f"Falied to fix error :(\nError occured while scanning {project['tags'][0]}")
 
 
+class ReadmeFilesTranslatonManager(NewDatasetManager):
+    def __init__(self, *args, **kwargs):
+        self.mapper = self.preprocess
+        self.maxLength = 10 # < 5000 for Google translator
+
+    def removeLinks(self, text):
+        pattern = re.compile(
+            r'\b(?:https?://|www\.)\S+\b',
+            re.IGNORECASE  # Ensures matching regardless of case for http/https/www
+        )
+        return pattern.sub('', text)
+
+    def removeMarkdown(self, mardown):
+        html = markdown.markdown(markdown)
+
+        soup = BeautifulSoup(html, "html.parser")
+
+        # remove code blocks and other elements
+        for pre in soup.find_all('pre'):
+            pre.extract()
+        # remove <code> tags
+        for code in soup.find_all('code'):
+            code.extract()
+
+        text = soup.get_text()
+
+        text = self.removeLinks(text)
+
+        # normalize whitespace
+        text = '\n'.join(line.strip() for line in text.splitlines() if line.strip())
+        return text
+
+    def truncate(self, text):
+        length = len(text)
+
+        while length > self.maxLength:
+            length = text.rfind("\n", 0, length)
+
+        if length <= 5:
+            return text[0:self.maxLength] # just cut off the rest of content
+        return text[0:length]
+
+    async def preprocess(self, loop, obj):
+        newObj = dict(obj)
+        readme = self.removeMarkdown(obj["readme"])
+        readme = self.truncate(readme)
+
+        translated = await loop.run_in_executor(
+            None,  # Use the default ThreadPoolExecutor
+            self.translateText,
+            readme
+        )
+
+        newObj["readme"] = translated
+
+        return newObj
+
+    async def runAsync(self):
+        try:
+            self.readInput()
+        except EXP_END_OF_DATA:
+            return EXP_END_OF_DATA
+
+        loop = asyncio.get_running_loop()
+
+        # Create a list of asynchronous tasks
+        tasks = []
+        for doc in self.data:
+            if self.validator(doc):
+                task = self.preprocess(loop, doc)
+                tasks.append(task)
+
+        results = await asyncio.gather(*tasks)
+
+        self.mappedData.extend(results)
+
+        self.writeOutput()
+
+    async def call(self):
+        # will execute the entire process of this manager, processing the entire dataset
+
+        while 1:
+            result = await self.runAsync()
+            if result == EXP_END_OF_DATA:
+                break
 
